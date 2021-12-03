c5fac80138ece3ae47aeddf7d09a2f0656ffcfc8
Angelos Nikolas <A.nikolas@newcastle.ac.uk>
2021-12-03 16:01:08 +0000

    Creation of pre-proccesing script and data contsruction regarding video stats

--- munge/03-A.R
+++ munge/03-A.R
@@ -3,3 +3,9 @@
 
 ##################################################################
 #Video stats pre-processing 
+sum(is.na(cyber.security.3_video.stats))
+
+############## DATA CONSTRUCTION##################################3
+video_views1 = select(cyber.security.3_video.stats, c(1:15))
+Video_devices1 = select(cyber.security.3_video.stats, c(16:21))
+video_location1 = select(cyber.security.3_video.stats, c(22:28))
