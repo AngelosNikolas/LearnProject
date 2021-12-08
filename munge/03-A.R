4c7b203fe2817d19a3a682c24c18ed67ebe7eb36
Angelos Nikolas <A.nikolas@newcastle.ac.uk>
2021-12-08 19:57:56 +0000

    Added the data construction script and did some alteration in the calculations

--- munge/03-A.R
+++ munge/03-A.R
@@ -43,29 +43,39 @@ UnknownEmployment4 = filter(cyber.security.3_enrolments, employment_status == "U
 ########################################################################################################################
 #Question.response data set prepossessing 
 
-#Remove empty column
-cyber.security.3_question.response = select(cyber.security.3_question.response, -c(cloze_response))
-
-# Check for NAs 
-sum(is.na(cyber.security.3_question.response))
-
-#############DATA CONSTRUCTION##############
-str(cyber.security.3_question.response)
-unique(cyber.security.3_question.response$quiz_question)
-str(cyber.security.3_question.response$quiz_question)
-
 # Extracting the correct answers regarding the 1st week
-Week1Correct3 = filter(cyber.security.3_question.response, step_number == 7)
-Week1Correct3 =  filter(cyber.security.3_question.response, correct == "true" )
+Week1Correct3 = filter(cyber.security.3_question.response, week_number == 1)
+Week1Correct3 =  filter(Week1Correct3, correct == "true" )
+
+# Extracting all the answers for week 1
+filter(cyber.security.3_question.response, week_number == 1)
+
+#Calculating percentage of completion
+Week1TruePercentage3 = (5783*0.1)/8.691
 
 # Extracting the correct answers regarding the 2st week
-Week2Correct3 = filter(cyber.security.3_question.response, step_number == 8)
-Week2Correct3 =  filter(cyber.security.3_question.response, correct == "true" )
+Week2Correct2 = filter(cyber.security.3_question.response, week_number == 2)
+Week2Correct2 =  filter(Week2Correct2, correct == "true" )
+
+# Extracting all the answers for week 1
+filter(cyber.security.3_question.response, week_number == 2)
+
+#Calculating percentage of completion
+Week2TruePercentage3 = (2170*100)/3969
 
 # Extracting the correct answers regarding the 3st week
-Week3Correct3 = filter(cyber.security.3_question.response, step_number == 11)
-Week3Correct3 =  filter(cyber.security.3_question.response, correct == "true" )
+Week3Correct2 = filter(cyber.security.3_question.response, week_number == 3)
+Week3Correct2 =  filter(Week3Correct2, correct == "true" )
 
+# Extracting all the answers for week 1
+filter(cyber.security.3_question.response, week_number == 3)
+
+#Calculating percentage of completion
+Week3TruePercentage3 = (1625*0.1)/3.860
+
+#Construction for the completion rates
+QuestionPercent3 = c(Week1TruePercentage3,Week2TruePercentage3,Week3TruePercentage3) 
+Question_Data3= data.frame(Weeks,QuestionPercent3)
 ######################################################################
 #Step activity pre-processing
 
@@ -79,24 +89,59 @@ sum(is.na(cyber.security.3_step.activity$learner_id))
 #####################DATA CONSTRUCTION######################################
 
 
+
+
 #Filter by week 1
 Week1Steps3 = filter(cyber.security.3_step.activity, week_number == 1)
-
-#Extract the number of how many didnt completed the steps
+#Extract the number of how many didn't completed the steps
 filter(Week1Steps3 , last_completed_at == "")
-#Extract the count of observations that completed step 1
-filter(Week1Steps3, step_number == 1 , !last_completed_at == "" )
-filter(Week1Steps3, step_number == 1 , last_completed_at == "" )
+#Extract the number of how many did not completed the steps and how many did
+filter(Week1Steps3, week_number == 1 , !last_completed_at == "" )
+filter(Week1Steps3, week_number == 1 , last_completed_at == "" )
+Week1Step3Perc = (19464*0.1)/22.307
 
 #Filter by week 2
-Week2Steps3 = filter(cyber.security.3_step.activity, week_number == 2)
-#Extract the number of how many did not completed the steps
-filter(Week2Steps3 , last_completed_at == "")
+Week2Steps2 = filter(cyber.security.3_step.activity, week_number == 2)
+#Extract the number of how many did not completed the steps and how many did
+filter(Week2Steps2, week_number == 2 , !last_completed_at == "" )
+filter(Week2Steps2, week_number == 2 , last_completed_at == "" )
+Week2Step3Perc = (13.835*100000)/14692
 
 #Filter by week 3
-Week3Steps3 = filter(cyber.security.3_step.activity, week_number == 3)
-#Extract the number of how many didnt completed the steps
-filter(Week3Steps3 , last_completed_at == "")
+Week3Steps2 = filter(cyber.security.3_step.activity, week_number == 3)
+#Extract the number of how many didn't completed the steps and how many did
+filter(Week3Steps2, week_number == 3 , !last_completed_at == "" )
+filter(Week3Steps2, week_number == 3 , last_completed_at == "" )
+Week3Step3Perc = (9.137*100000)/9615
+
+#Creation of new data frames for analysis
+Step_percentages3 = c(Week1Step3Perc,Week2Step3Perc,Week3Step3Perc)
+Step_data3 = data.frame(Step_Weeks,Step_percentages3)
+
+#Categorizing the step categories and calculating percentages of completion 
+Videos3 = filter(cyber.security.3_step.activity, step %in% c("101","104","113","116","118","201","204","216","301","302","314","315"))
+filter(Videos3, !last_completed_at == "" )
+VideosPerc3 = (9.218*100000)/10590
+
+Articles3 = filter(cyber.security.3_step.activity, step %in% c("102","105","106","108","111","112","114","115","117","202","205","207","208","210","211","212","213","214","215","217","218","220","305","306","307","308","309","310","313","317","320","321"))
+filter(Articles3 , !last_completed_at == "" )
+ArticlesPerc3 = (21.092*100000)/22880
+
+Discussions3 = filter(cyber.security.3_step.activity, step %in% c("109","110","206","221","304","312","316","319"))
+filter(Discussions3 , !last_completed_at == "" )
+DiscussionsPerc3 = (4987*100)/5336
+
+Quizes3 = filter(cyber.security.3_step.activity, step %in% c("107","208","219","311","318"))
+filter(Quizes3, !last_completed_at == "" )
+QuizesPerc3 = (2.778*100000)/2930
+
+Exercises3 = filter(cyber.security.3_step.activity, step %in% c("103","203","303"))
+filter(Exercises3, !last_completed_at == "" )
+ExercisesPerc3 = (2555*100)/2894
+
+#Construction for the step types
+Step_Completion3 = c(VideosPerc3,ArticlesPerc3,DiscussionsPerc3,QuizesPerc3,ExercisesPerc3)
+Step_Types_data3 = data.frame(Step_Types,Step_Completion3)
 
 
 
